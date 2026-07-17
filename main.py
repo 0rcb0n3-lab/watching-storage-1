3a8364cdb96ca413314215c28e93a99c961ba762
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-07-17 17:12:31 +0300

    get card details

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     # print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    print(Passcard.objects.all())
 
+    passcards = Passcard.objects.all()
+    some_card = passcards[1]
 
-
-
+    print("owner_name: ", some_card.owner_name)
+    print("passcode: ", some_card.passcode)
+    print("created_at: ", some_card.created_at)
+    print("is_active: ", some_card.is_active)
