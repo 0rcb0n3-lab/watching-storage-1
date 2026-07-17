0174c070938c5d14b645e434879eddf05c5a0e6a
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-07-17 17:28:55 +0300

    get active cards

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    # print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
 
     passcards = Passcard.objects.all()
-    some_card = passcards[1]
 
-    print("owner_name: ", some_card.owner_name)
-    print("passcode: ", some_card.passcode)
-    print("created_at: ", some_card.created_at)
-    print("is_active: ", some_card.is_active)
+    active_passcards = [card for card in passcards if card.is_active]
+
+    print('Количество активных пропусков:', len(active_passcards))
